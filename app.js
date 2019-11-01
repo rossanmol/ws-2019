599c48d4e7f64751fa9c9eb632041d9c62f9fa15
Rostislav Dascal <rostislav.dascal@betssongroup.com>
2019-11-01 10:36:38 +0100

    app

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-// Import net module.
-var net = require('net');
+const net = require('net');
+const port = 7070;
+const host = '127.0.0.1';
 
-var server = net.createServer();
-server.listen('127.0.0.1', 5222);
-console.log('Server listening on ' + server.address().address + ':' + server.address().port);
-server.on('connection', function (sock) {
-  console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-  // other stuff is the same from here
+const server = net.createServer();
+server.listen(port, host, () => {
+  console.log('TCP Server is running on port ' + port + '.');
 });
