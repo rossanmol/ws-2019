97d85db55461ab1546def94fd84bb08d1022507d
Rostislav Dascal <rostislav.dascal@betssongroup.com>
2019-11-01 09:09:50 +0100

    app

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-var ws = require("nodejs-websocket")
+const WebSocket = require('ws');
 
-// Scream server example: "hi" -> "HI!!!"
-var server = ws.createServer(function (conn) {
-  console.log("New connection")
-  conn.on("text", function (str) {
-    console.log("Received " + str)
-    conn.sendText(str.toUpperCase() + "!!!")
-  })
-  conn.on("close", function (code, reason) {
-    console.log("Connection closed")
-  })
-}).listen(process.env.PORT || 3000)
+const wss = new WebSocket.Server({
+  port: process.env.PORT || 3000,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed.
+  }
+});
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(`received ${data}`);
+      }
+    });
+  });
+});
