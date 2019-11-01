d08dc0d5a2202b79e2dfb51d0d7d9f589dd82a76
Rostislav Dascal <rostislav.dascal@betssongroup.com>
2019-11-01 10:26:19 +0100

    app

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
+// Import net module.
 var net = require('net');
 
-var HOST = '127.0.0.1';
-var PORT = process.env.PORT | 5222;
+// Create and return a net.Server object, the function will be invoked when client connect to this server.
+var server = net.createServer(function (client) {
 
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-net.createServer(function (sock) {
-  // We have a connection - a socket object is assigned to the connection automatically
-  console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-  // Add a 'data' event handler to this instance of socket
-  sock.on('data', function (data) {
-    console.log('DATA ' + sock.remoteAddress + ': ' + data);
-    // Write the data back to the socket, the client will receive it as data from the server
-    sock.write('You said "' + data + '"');
+  console.log('Client connect. Client local address : ' + client.localAddress + ':' + client.localPort + '. client remote address : ' + client.remoteAddress + ':' + client.remotePort);
+
+  client.setEncoding('utf-8');
+
+  client.setTimeout(1000);
+
+  // When receive client data.
+  client.on('data', function (data) {
+
+    // Print received client data and length.
+    console.log('Receive client send data : ' + data + ', data size : ' + client.bytesRead);
+
+    // Server send data back to client use client net.Socket object.
+    client.end('Server received data : ' + data + ', send back to client data size : ' + client.bytesWritten);
   });
-  // Add a 'close' event handler to this instance of socket
-  sock.on('close', function (data) {
-    console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+
+  // When client send data complete.
+  client.on('end', function () {
+    console.log('Client disconnect.');
+
+    // Get current connections count.
+    server.getConnections(function (err, count) {
+      if (!err) {
+        // Print current connection count in server console.
+        console.log("There are %d connections now. ", count);
+      } else {
+        console.error(JSON.stringify(err));
+      }
+
+    });
   });
 
-}).listen(PORT, HOST);
+  // When client timeout.
+  client.on('timeout', function () {
+    console.log('Client request time out. ');
+  })
+});
 
-console.log('Server listening on ' + HOST + ':' + PORT);
+// Make the server a TCP server listening on port 9999.
+server.listen(9999, function () {
+
+  // Get server address info.
+  var serverInfo = server.address();
+
+  var serverInfoJson = JSON.stringify(serverInfo);
+
+  console.log('TCP server listen on address : ' + serverInfoJson);
+
+  server.on('close', function () {
+    console.log('TCP server socket is closed.');
+  });
+
+  server.on('error', function (error) {
+    console.error(JSON.stringify(error));
+  });
+
+});
